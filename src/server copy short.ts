652e247f785a30a2d9fb2280c08816900762cba5
Gemma <ggc.gemma@gmail.com>
2025-02-10 21:17:00 +0100

    fixing error in netlify

--- src/server copy short.ts
+++
@@ -1,8 +0,0 @@
-import { CommonEngine } from "@angular/ssr/node";
-import { render } from "@netlify/angular-runtime/common-engine";
-
-const commonEngine = new CommonEngine();
-
-export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-  return await render(commonEngine);
-}
